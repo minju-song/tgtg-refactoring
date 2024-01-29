6f1117a076658c5332203bd183b89106513e3ec4
송민주 <songjaeskkk@naver.com>
2024-01-29 17:15:02 +0900

    init

---
+++ tgtg/src/main/java/com/malzzang/tgtg/TgtgApplication.java
@@ -0,0 +1,13 @@
+package com.malzzang.tgtg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgtgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgtgApplication.class, args);
+	}
+
+}
---
+++ tgtg/src/test/java/com/malzzang/tgtg/TgtgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malzzang.tgtg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgtgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
