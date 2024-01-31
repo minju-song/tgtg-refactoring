27776b730db72b7689b37f789dc48a2cc294d8c4
ljw03079 <ljw03079@naver.com>
2024-01-31 19:05:11 +0900

    Entity 생성2

---
+++ tgtg/src/main/java/com/malzzang/tgtg/subject/SubjectRepository.java
@@ -0,0 +1,7 @@
+package com.malzzang.tgtg.subject;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubjectRepository extends JpaRepository<Subject, Integer> {
+	
+}
