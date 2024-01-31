715fd570c618e7781bbc01c031f16a3188b96d6a
송민주 <songjaeskkk@naver.com>
2024-01-31 19:22:22 +0900

    memberEntity 수정

--- tgtg/src/main/java/com/malzzang/tgtg/member/Member.java
+++ tgtg/src/main/java/com/malzzang/tgtg/member/Member.java
@@ -14,7 +14,6 @@ import lombok.Data;
 public class Member {
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name="member_id")
 	private String memberId;
 	
--- tgtg/src/main/java/com/malzzang/tgtg/test/testController.java
+++ tgtg/src/main/java/com/malzzang/tgtg/test/testController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.malzzang.tgtg.forbid.Forbid;
+import com.malzzang.tgtg.forbid.ForbidRepository;
 import com.malzzang.tgtg.test2.test2;
 import com.malzzang.tgtg.test2.test2Repository;
 
@@ -15,42 +17,18 @@ public class testController {
 	
 	private final testRepository repository;
 	private final test2Repository repository2;
+	private final ForbidRepository frepo;
 	
-	public testController(testRepository r1, test2Repository r2) {
+	public testController(testRepository r1, test2Repository r2, ForbidRepository f) {
         this.repository = r1;
         this.repository2 = r2;
+        this.frepo = f;
     }
 	
 	@GetMapping("/test")
     public void addTest() {
-		test t = new test();
-
-		t.setName("수민");
-		t.setEmail("bbb");
-		repository.save(t);
-		
-		List<test> result = new ArrayList<>();
-		result = repository.findAll();
-		for(int i = 0; i < result.size(); i++) {
-			System.out.println(result.get(i));
-		}
-		
-		test2 t2 = new test2();
-
-		t2.setId(5);
-		t2.setT(t);
-		t2.setSchool("아무고");
-		repository2.save(t2);
-		
-		test2 t3 = new test2();
-		t3.setT(t);
-		t3.setSchool("아무대");
-		repository2.save(t3);
-		
-		List<test2> result2 = new ArrayList<>();
-		result2 = repository2.findAll();
-		for(int i = 0; i < result2.size(); i++) {
-			System.out.println(result2.get(i));
-		}
+		Forbid forVO = new Forbid();
+		forVO.setForbidWord("금지어");
+		frepo.save(forVO);
     }
 }
