370cd46193c57b2622c58c1f329a645b66aea714
mereminiGit <lkygmin74@gmail.com>
2024-02-07 22:37:01 +0900

    헤더 분리 및 암호화 진

--- tgtg/src/main/resources/static/js/functions.js
+++ tgtg/src/main/resources/static/js/functions.js
@@ -151,29 +151,29 @@ $( document ).ready(function() {
 
   }
 
-  function outerNav() {
+  // function outerNav() {
 
-    $('.header--nav-toggle').click(function(){
+  //   $('.header--nav-toggle').click(function(){
 
-      $('.perspective').addClass('perspective--modalview');
-      setTimeout(function(){
-        $('.perspective').addClass('effect-rotate-left--animate');
-      }, 25);
-      $('.outer-nav, .outer-nav li, .outer-nav--return').addClass('is-vis');
+  //     $('.perspective').addClass('perspective--modalview');
+  //     setTimeout(function(){
+  //       $('.perspective').addClass('effect-rotate-left--animate');
+  //     }, 25);
+  //     $('.outer-nav, .outer-nav li, .outer-nav--return').addClass('is-vis');
 
-    });
+  //   });
 
-    $('.outer-nav--return, .outer-nav li').click(function(){
+  //   $('.outer-nav--return, .outer-nav li').click(function(){
 
-      $('.perspective').removeClass('effect-rotate-left--animate');
-      setTimeout(function(){
-        $('.perspective').removeClass('perspective--modalview');
-      }, 400);
-      $('.outer-nav, .outer-nav li, .outer-nav--return').removeClass('is-vis');
+  //     $('.perspective').removeClass('effect-rotate-left--animate');
+  //     setTimeout(function(){
+  //       $('.perspective').removeClass('perspective--modalview');
+  //     }, 400);
+  //     $('.outer-nav, .outer-nav li, .outer-nav--return').removeClass('is-vis');
 
-    });
+  //   });
 
-  }
+  // }
 
   function workSlider() {
 
