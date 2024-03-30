80b0b7d6d8d0ac747566ba466c8e48ba2b6e8f7f
mereminiGit <lkygmin74@gmail.com>
2024-03-31 01:26:20 +0900

    헤더 수정, 메인 순위, 메인 푸터 완료

--- tgtg/src/main/java/com/malzzang/tgtg/HomeController.java
+++ tgtg/src/main/java/com/malzzang/tgtg/HomeController.java
@@ -1,10 +1,17 @@
 package com.malzzang.tgtg;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.malzzang.tgtg.mail.MailService;
 import com.malzzang.tgtg.member.service.MemberService;
 
 
@@ -14,10 +21,29 @@ public class HomeController {
 	@Autowired
 	MemberService memberService;
 	
+	@Autowired
+	MailService mailService;
+	
+	@Value("${admin.mail}")
+	private String mail;
+	
 	@GetMapping("/")
 	public String home(Model model) {
 		model.addAttribute("memberWin", memberService.selectMemberWin());
 		System.out.println(memberService.selectMemberWin());
 		return "index.html";
+	}
+	
+	@PostMapping("/contactEmail")
+	@ResponseBody
+	public String contactEmail(@RequestBody Map<String, Object> contact) throws Exception {
+		System.out.println(contact);
+		
+		Boolean result = mailService.sendSimpleMessage(mail, (String)contact.get("emailEmail"), (String)contact.get("emailName"), (String)contact.get("emailContent"));
+		
+		if(result) {
+			return "success";
+		}
+		return "fail";
 	}
 }
