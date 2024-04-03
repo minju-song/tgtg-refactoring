5d3d9dd7b62ee43fbb2cd17bd82d70369bc3960d
송민주 <songjaeskkk@naver.com>
2024-04-04 08:09:47 +0900

    Merge branch 'main' of https://github.com/TiGyeog-TaeGyeog/tgtgProject.git into HEAD

--- tgtg/src/test/java/com/malzzang/tgtg/TgtgApplicationTests.java
+++ tgtg/src/test/java/com/malzzang/tgtg/TgtgApplicationTests.java
@@ -4,23 +4,30 @@ import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootTest
 class TgtgApplicationTests {
+	
+	private static final Logger logger = LoggerFactory.getLogger(TgtgApplicationTests.class);
 
 	@Autowired
 	   StringEncryptor jasyptStringEncryptor;
 	   
 	@Test
 	   void encryptor() {
-	      String[] datas = {
-
-	      };
+		
+		String[] datas = {
+	            "tgtg"
+        };
 	      
-	      for(String data : datas) {
-	         String encData = jasyptStringEncryptor.encrypt(data);
-	         System.out.println(encData);
-	      }
-	   }
-
+        for(String data : datas) {
+            String encData = jasyptStringEncryptor.encrypt(data);
+            System.out.println("Original: " + data + " Encrypted: " + encData);
+        }
+	}
+	
 }
+
+
